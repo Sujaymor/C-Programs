e8599b0a8b1f774d23faa8a728acd6f3484e63c3
Shashikll <112857948+Shashikll@users.noreply.github.com>
2022-09-05 14:19:43 +0530

    Add files via upload

---
+++ area of rectangle.c
@@ -0,0 +1,20 @@
+//Compiler version gcc  6.3.0
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   int l, b, a;
+   printf("Enter Your Length");
+   scanf("%d",&a);
+   printf("Enter Your Breadth ");
+   scanf("%d",&b);
+    a=l*b;
+    b=l*b;
+   printf("Your rectangular area is %d ",a,b);
+   
+   printf("\n Thanks!!");
+   
+   getch();
+   return 0;
+   aa
+}
