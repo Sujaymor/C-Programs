1e661f4f8db9b1fb91083446ee943e6e871fda83
Shashikll <112857948+Shashikll@users.noreply.github.com>
2022-09-27 19:57:34 +0530

    Add files via upload

---
+++ Basic Program/Multiplication.c
@@ -0,0 +1,21 @@
+
+//Compiler version gcc  6.3.0
+
+#include <stdio.h>
+#include <stdio.h>
+
+ int main()
+ {
+     int No1 = 0, No2 = 0, Sum = 0 ;
+     
+     printf("\n\n\n Enter a two numbers = ");
+     scanf("%d%d",&No1,&No2);
+     
+     Sum = No1 * No2;
+     
+     printf("\n Multiplication  of %d _ %d = %d.",No1, No2, Sum);
+     
+     getch ();
+     return 0;
+ 
+ }  
