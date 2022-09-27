a705dd8e4beae2f249ad8c8e0d8896c2075d951f
Shashikll <112857948+Shashikll@users.noreply.github.com>
2022-09-27 19:45:21 +0530

    Add files via upload

---
+++ Pattern Printing/04 pattern Printing N.c
@@ -0,0 +1,33 @@
+//Compiler version gcc  6.3.0
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+      int r = 0, c = 0, cnt = 0;
+  
+      printf("\n Enter a number => ");
+      scanf("%d",&cnt);
+      
+      for(r=1; r<=cnt; r++)
+      {
+        for(c=1; c<=cnt; c++)
+        {
+          if(r==c || c==1 || c==cnt)
+          {
+            printf(" * ");
+          }
+          else
+          {
+            printf("   ");
+          }
+        }
+        printf("\n");
+      }
+      
+      getch();
+      return 0;
+      
+}
+  
+  
