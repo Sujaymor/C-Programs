db9c2f1278961108d9f486092aa22a40946e21b8
Shashikant yadav <112857948+Shashikll@users.noreply.github.com>
2022-10-07 22:05:31 +0530

    Add files via upload

---
+++ Looping statement/01 Prg To Print Name On first Line And Sunname On Second Lines 5 Times.c
@@ -0,0 +1,18 @@
+//Compiler version gcc  6.3.0
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{     
+       int Cnt = 0;
+       
+       for(Cnt = 1; Cnt <= 5; Cnt++)
+       {
+         printf("Shashikant Yadav\n\n");
+       }
+      
+       printf("\n\n Thanks... ");
+  
+       getch();
+       return 0;
+}
