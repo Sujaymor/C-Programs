e941ffd12ef89eb5e00858d148a55aee6540ae66
Shashikant yadav <112857948+Shashikll@users.noreply.github.com>
2022-10-08 18:35:50 +0530

    Add files via upload

---
+++ Looping statement/04 Print Square Of Given Number.c
@@ -0,0 +1,17 @@
+//Compiler version gcc  6.3.0
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+     int No = 0;
+     
+     printf("\n Enter Number => ");
+     scanf("%d",&No);
+    
+     printf("\n Square Of %d = %d",No,(No * No));
+     printf("\n\n Thanks... ");
+     
+     getch();
+     return 0;
+}
